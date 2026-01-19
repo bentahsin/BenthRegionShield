1061c6beedd7a7595b9c286d37f7d788c14f1a40
bentahsin <tahsin@bentahsin.com>
2026-01-19 23:03:25 +0300

    Update Java version to 17 and refactor WorldGuard integration for improved compatibility

--- src/main/java/com/bentahsin/regionshield/BenthRegionShield.java
+++ src/main/java/com/bentahsin/regionshield/BenthRegionShield.java
@@ -13,6 +13,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -92,7 +93,7 @@ public class BenthRegionShield {
 
         ShieldCacheKey cacheKey = new ShieldCacheKey(
                 player.getUniqueId(),
-                location.getWorld().getName(),
+                world.getName(),
                 location.getBlockX(),
                 location.getBlockY(),
                 location.getBlockZ(),
--- src/main/java/com/bentahsin/regionshield/hooks/worldguard/WorldGuardHook.java
+++ src/main/java/com/bentahsin/regionshield/hooks/worldguard/WorldGuardHook.java
@@ -6,19 +6,16 @@ import com.bentahsin.regionshield.api.ShieldResponse;
 import com.bentahsin.regionshield.internal.ReflectionUtils;
 import com.bentahsin.regionshield.model.InteractionType;
 import com.bentahsin.regionshield.model.RegionInfo;
-import com.sk89q.worldedit.world.World;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
-import java.util.ArrayList;
-
-/**
- * WorldGuard Entegrasyonu.
- * Sunucu sürümünü algılar ve uygun işçiyi (Worker V6 veya V7) devreye sokar.
- */
 public class WorldGuardHook implements IShieldHook {
 
-    private IWorldGuardWorker worker;
+    private final IWorldGuardWorker worker;
+
+    public WorldGuardHook() {
+        this.worker = new WorldGuard7Worker();
+    }
 
     @Override
     public String getName() {
@@ -27,27 +24,12 @@ public class WorldGuardHook implements IShieldHook {
 
     @Override
     public boolean canInitialize() {
-        if (!ReflectionUtils.isPluginActive("WorldGuard")) return false;
-
-        if (ReflectionUtils.getClass("com.sk89q.worldguard.WorldGuard") != null) {
-            this.worker = new WorldGuard7Worker();
-            return true;
-        }
-
-        if (ReflectionUtils.getClass("com.sk89q.worldguard.bukkit.WGBukkit") != null) {
-            this.worker = new WorldGuard6Worker();
-            return true;
-        }
-
-        return false;
+        return ReflectionUtils.isPluginActive("WorldGuard");
     }
 
     @Override
     public ShieldResponse check(Player player, Location location, InteractionType type) {
-        if (worker == null) return ShieldResponse.allow();
-
         boolean allowed = worker.canBuild(player, location, type);
-
         return allowed ? ShieldResponse.allow() : ShieldResponse.deny(getName());
     }
 
@@ -61,7 +43,7 @@ public class WorldGuardHook implements IShieldHook {
         try {
             com.sk89q.worldedit.util.Location weLoc = com.sk89q.worldedit.bukkit.BukkitAdapter.adapt(location);
             com.sk89q.worldguard.protection.regions.RegionContainer container = com.sk89q.worldguard.WorldGuard.getInstance().getPlatform().getRegionContainer();
-            com.sk89q.worldguard.protection.managers.RegionManager regions = container.get((World) weLoc.getExtent());
+            com.sk89q.worldguard.protection.managers.RegionManager regions = container.get((com.sk89q.worldedit.world.World) weLoc.getExtent());
 
             if (regions == null) return null;
 
@@ -74,12 +56,17 @@ public class WorldGuardHook implements IShieldHook {
             return RegionInfo.builder()
                     .id(region.getId())
                     .provider(getName())
-                    .owners(new ArrayList<>(region.getOwners().getUniqueIds()))
-                    .members(new ArrayList<>(region.getMembers().getUniqueIds()))
+                    .owners(new java.util.ArrayList<>(region.getOwners().getUniqueIds()))
+                    .members(new java.util.ArrayList<>(region.getMembers().getUniqueIds()))
                     .build();
 
         } catch (Exception e) {
             return null;
         }
+    }
+
+    @Override
+    public com.bentahsin.regionshield.model.RegionBounds getRegionBounds(Location location) {
+        return worker.getRegionBounds(location);
     }
 }
